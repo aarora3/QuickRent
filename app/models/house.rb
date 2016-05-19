29f183510ee51f7c5732fd14f4977a169bb1196a
Ankush Arora <aarora3@binghamton.edu>
2016-05-19 00:30:32 +0000

    post ad features

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,6 +1,16 @@
 class House < ActiveRecord::Base
+    #validates :title, presence: true
     has_many :reviews
     has_many :users, :through => :reviews
+    
+    #before_save { self.email = email.downcase }
+    #validates :name,  presence: true, length: { maximum: 50 }
+    #VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    #validates :email, presence: true, length: { maximum: 255 },
+    #                format: { with: VALID_EMAIL_REGEX },
+    #                uniqueness: { case_sensitive: false }
+    #validates :description, presence: true, length: { minimum: 5 }
+    
     
     def self.ratings 
         ['Vestal','Binghamton','Downtown','Endicott']
